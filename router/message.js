55b5a4f294a2770de68e6f44f4a07cb0c324c0a1
7985575255 <arjun21217@gmail.com>
2023-04-11 19:53:37 +0530

    This message for controlers how is working

--- router/message.js
+++ router/message.js
@@ -1,30 +1,12 @@
 const express=require('express');
 const router=express.Router();
 const fs=require('fs');
+
+const messsageControler=require('../controlers/message')
+const reseveMessage=require('../controlers/message');
 const body_parser=require('body-parser');
 router.use(body_parser.urlencoded({extended:false}))
 
-router.get('/',(req,res, next)=>{
-    fs.readFile('username.txt', (err,data)=>{
-        if(err){
-            data='No chat found'
-        }
-         res.send(`${data}<form action="/" method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" >
-    <input type="text" name="message" id="message" placeHolder="Message">
-    <input type="hidden" name="username" id="username">
-     <button type="submit">submit</button>
-     </form>`)
-    })
-   
-})
-router.post('/',(req,res,next)=>{
-    console.log(req.body.username)
-    console.log(req.body.message)
-fs.writeFile("username.txt",`${req.body.username} -${req.body.message}`,{flag:'a'},(err)=>{
-    if(err){
-        console.log(err);
-    }
-    res.redirect('/')
-})
-})
+router.get('/',messsageControler.getAddMessage)
+//router.post('/',reseveMessage.sendMessage);
 module.exports=router;
